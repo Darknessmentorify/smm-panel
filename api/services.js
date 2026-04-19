ceda958efd689c43080e7c8e2cae53491fd120e4
Darknessmentorify <guillermoherrera435@gmail.com>
2026-04-18 22:51:31 -0600

    Update services.js

--- api/services.js
+++ api/services.js
@@ -2,28 +2,23 @@ export default async function handler(req, res) {
 
   const API_KEY = "TU_API_KEY_AQUI";
 
-  try {
-    const response = await fetch("https://socialmentorify.com/api/v2", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        key: API_KEY,
-        action: "services"
-      })
-    });
+  const response = await fetch("https://socialmentorify.com/api/v2", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      key: API_KEY,
+      action: "services"
+    })
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    const mod = data.map(s => ({
-      ...s,
-      rate: (parseFloat(s.rate) * 2).toFixed(2)
-    }));
+  const mod = data.map(s => ({
+    ...s,
+    rate: (parseFloat(s.rate) * 2).toFixed(2)
+  }));
 
-    res.status(200).json(mod);
-
-  } catch (e) {
-    res.status(500).json({ error: "Error API" });
-  }
+  res.status(200).json(mod);
 }
